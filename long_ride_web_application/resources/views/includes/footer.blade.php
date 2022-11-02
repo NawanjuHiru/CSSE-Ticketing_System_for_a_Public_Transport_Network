247f8c140539f41a44ab615d2125a1e9cb0bcdce
HirushaRukmal <hirusharukmal266@gmail.com>
2022-11-02 15:59:40 +0530

    Changes to Footer

--- long_ride_web_application/resources/views/includes/footer.blade.php
+++ long_ride_web_application/resources/views/includes/footer.blade.php
@@ -1,7 +1,7 @@
 <!-- Footer opened -->
 <div class="main-footer ht-40">
     <div class="container-fluid pd-t-0-f ht-100p">
-        <span>Copyright © 2021  Designed & developed by <a href="/">Flex</a> All rights reserved.</span>
+        <span>Copyright © 2022 Designed & developed by <a href="/">SE3070_WE_02</a> All rights reserved.</span>
     </div>
 </div>
 <!-- Footer closed -->
